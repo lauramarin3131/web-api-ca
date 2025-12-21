66a34ba63ee893598355b024ef28135b6694d8d9
lauramarin3131 <lauramarin3131@gmail.com>
2025-12-21 13:49:32 +0100

    add additional endpoints

--- movies-api/api/movies/index.js
+++ movies-api/api/movies/index.js
@@ -1,6 +1,12 @@
 import express from 'express';
 import asyncHandler from 'express-async-handler';
-import { getMovies } from '../tmdb-api'; 
+import {
+  getMovies,
+  getMovieById,
+  getUpcomingMovies,
+  getGenres,
+} from "../tmdb-api";
+
 
 const router = express.Router();
 
@@ -9,5 +15,31 @@ router.get('/discover', asyncHandler(async (req, res) => {
     const discoverMovies = await getMovies();
     res.status(200).json(discoverMovies);
 }));
+
+router.get(
+  "/upcoming",
+  asyncHandler(async (req, res) => {
+    const upcoming = await getUpcomingMovies();
+    res.status(200).json(upcoming);
+  })
+);
+
+router.get(
+  "/genres",
+  asyncHandler(async (req, res) => {
+    const genres = await getGenres();
+    res.status(200).json(genres);
+  })
+);  
+
+router.get(
+  "/:id",
+  asyncHandler(async (req, res) => {
+    const movie = await getMovieById(req.params.id);
+    res.status(200).json(movie);
+  })
+);
+
+
 
 export default router;
--- movies-api/api/tmdb-api.js
+++ movies-api/api/tmdb-api.js
@@ -11,3 +11,41 @@ export const getMovies = async () => {
 
     return await response.json();
 };
+
+export const getUpcomingMovies = async () => {
+  const response = await fetch(
+    `https://api.themoviedb.org/3/movie/upcoming?api_key=${process.env.TMDB_KEY}&language=en-US&page=1`
+  );
+
+  if (!response.ok) {
+    throw new Error("Failed to fetch upcoming movies");
+  }
+
+  return await response.json();
+};
+
+
+export const getGenres = async () => {
+  const response = await fetch(
+    `https://api.themoviedb.org/3/genre/movie/list?api_key=${process.env.TMDB_KEY}&language=en-US`
+  );
+
+  if (!response.ok) {
+    throw new Error("Failed to fetch genres");
+  }
+
+  return await response.json();
+};
+
+export const getMovieById = async (id) => {
+  const response = await fetch(
+    `https://api.themoviedb.org/3/movie/${id}?api_key=${process.env.TMDB_KEY}&language=en-US`
+  );
+
+  if (!response.ok) {
+    throw new Error("Failed to fetch movie details");
+  }
+
+  return await response.json();
+};
+
